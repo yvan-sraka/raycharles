2205cf3fa269fb07ac1b964aa73725b9c60b0818
Adrien Zinger <azinger@alpenite.com>
2018-08-20 20:19:19 +0100

    Draw 3d

--- src/camera/view_wall.rs
+++ src/camera/view_wall.rs
@@ -1,8 +1,9 @@
 use crate::elements::tools::map_value;
 use crate::statics::limits::VISION;
 use crate::statics::limits::LIMITS;
-use crate::statics::limits::LIMITS_2D;
+//use crate::statics::limits::LIMITS_2D;
 use crate::statics::colors::WALL;
+use crate::statics::colors::FLOOR;
 
 use std::cmp::min;
 use std::cmp::max;
@@ -31,14 +32,34 @@ fn dist_2d(a: &Point, b: &Point) -> f64 {
   sqr.sqrt()
 }
 
+fn fill_rect_from_center(x: i32, w: u32, h: u32, color: Color, canvas: &mut Canvas<Window>) {
+  //let mut y = 0;
+  let middle = LIMITS.1 / 2;
+  let h2 = h / 2;
+  //if x < LIMITS_2D.2 as i32 {
+  //  y = LIMITS_2D.3 as i32;
+  //}
+  //let y2 = h2 - y as u32;
+  canvas.set_draw_color(FLOOR);
+  canvas.fill_rect(Rect::new(x, 0, w, (middle - h2) as u32)).expect("Error writting rect");
+  canvas.set_draw_color(color);
+  canvas.fill_rect(Rect::new(x, (middle - h2) as i32, w, h2)).expect("Error writting rect");
+  canvas.fill_rect(Rect::new(x, middle as i32, w, h2)).expect("Error writting rect");
+  canvas.set_draw_color(FLOOR);
+  canvas.fill_rect(Rect::new(x, (middle + h2) as i32, w, (LIMITS.1 - middle - h2) as u32)).expect("Error writting rect");
+}
+
+fn draw_wall_slice(x: i32, w: f32, dist: f64, canvas: &mut Canvas<Window>) {
+  let n = w / 10.0;
+  for i in 0..10 {
+    let offset = i as f32 * n;
+    fill_rect_from_center(x + offset as i32, (w + offset) as u32, LIMITS.1 / dist as u32 * 4, attenuation(dist), canvas);
+  }
+}
+
 pub fn draw(a: &Point, b: &Point, angle: f32, canvas: &mut Canvas<Window>) {
   let pa = angle + VISION / 2.0;
   let x = map_value(pa as u32, 0, VISION as u32, 0, LIMITS.0);
   let w = (LIMITS.0 as f32 / VISION) + 1.;
-  canvas.set_draw_color(attenuation(dist_2d(a, b)));
-  let mut y = 0;
-  if x < LIMITS_2D.2 {
-    y = LIMITS_2D.3;
-  }
-  canvas.fill_rect(Rect::new(x as i32, y as i32, w as u32, LIMITS.1)).expect("Error writting rect");
+  draw_wall_slice(x as i32, w, dist_2d(a, b), canvas);
 }
